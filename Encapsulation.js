fbe3e43ed38eb41e6b9c776aa3be4a419a318f99
home <djr52@njit.edu>
2020-02-21 17:27:23 -0500

    Finished encapsulation and its tests

---
+++ Encapsulation.js
@@ -0,0 +1,16 @@
+class  Encapsulation{
+    RoomOne(){
+        let desks = 5;
+        return desks;
+
+    }
+
+    Hallway(){
+        desks = 2;
+        return desks;
+
+    }
+
+
+}
+module.exports = Encapsulation;
---
+++ test-files/encapsulation.test.js
@@ -0,0 +1,16 @@
+const Encapsulation = require("../Encapsulation");
+test("Encapsulation RoomOne returns 5", () =>{
+    let Enc = new Encapsulation();
+    expect(Enc.RoomOne()).toBe(5);
+
+}
+);
+test("Encapsulation Hallway throws reference error", () =>{
+        let Enc = new Encapsulation();
+        expect(()=>{
+            Enc.Hallway()
+        }).toThrow();
+        //expect(Enc.Hallway()).toBe(2);
+
+    }
+);
